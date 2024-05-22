12b45dd8ba55bc56f4db5aaa023b8ad3e18863cf
Ubuntu <ubuntu@ip-172-31-22-247.ap-southeast-1.compute.internal>
2024-05-22 06:37:22 +0000

    updates from server

--- gps/asgi.py
+++ gps/asgi.py
@@ -13,13 +13,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from api.websocket.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gps.settings')
+django_asgi_app = get_asgi_application()
+from api.websocket.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 websocket_urlpatterns
--- gps/settings.py
+++ gps/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_extensions',
     'channels',
-    "daphne",
+#    "daphne",
     'django.contrib.staticfiles',
 ]
 
